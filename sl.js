e3d50b1c99f45f34a58c22268fdd185c20a83811
Marc Durdin <marc@durdin.net>
2020-06-03 14:13:49 +1000

    test

--- sl.js
+++ sl.js
@@ -2,8 +2,10 @@ let fs = require('fs');
 const longpath = '1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890';
 const p1 = 'module_1_'+longpath, p2 = 'module_2_'+longpath;
 
-fs.mkdirSync(p1);
-fs.mkdirSync(p2);
+if(!fs.existsSync(p1))
+  fs.mkdirSync(p1);
+if(!fs.existsSync(p2))
+  fs.mkdirSync(p2);
 fs.writeFileSync(p1 + '/foo', 'foo');
 fs.writeFileSync(p2 + '/foo', 'foo');
 fs.symlinkSync(p1, p2+'/'+p1, 'junction');
